28feef8c0b55c93b3cf44c11c04d0106e3f78a97
mike v <xvx1138@protonmail.com>
2019-07-19 18:21:43 +0200

    update

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 function computerPlay() {
 
 }
+
+function playRound(playerSelection, computerSelection) {
+	// your code here!
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
